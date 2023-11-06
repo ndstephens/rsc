4838307b9f6b375da2d4896fbf58aeafa1b7e592
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-11-06 17:06:52 -0500

    add pending indicator to search input using useTransition

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { useTransition } from 'react';
 import { useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 
+import { Spinner } from '@/components/spinner';
+
 export function SearchInput({ search }: { search: string }) {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
 
   return (
     <div className="relative mt-1 rounded-md shadow-sm">
@@ -18,17 +23,27 @@ export function SearchInput({ search }: { search: string }) {
         type="text"
         name="search"
         id="search"
-        className="block w-full rounded-md border-gray-300 pl-10 text-sm focus:border-gray-400 focus:outline-none focus:ring-0"
+        className="block w-full rounded-md border-gray-300 px-10 text-sm focus:border-gray-400 focus:outline-none focus:ring-0"
         placeholder="Search"
         defaultValue={search}
         onChange={(event) => {
-          if (event.target.value.trim()) {
-            router.push(`/?search=${event.target.value.trim()}`);
-          } else {
-            router.push('/');
-          }
+          startTransition(() => {
+            if (event.target.value.trim()) {
+              router.push(`/?search=${event.target.value.trim()}`);
+            } else {
+              router.push('/');
+            }
+          });
         }}
       />
+      {isPending && (
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <Spinner
+            className="w-5 animate-spin text-gray-400"
+            aria-hidden="true"
+          />
+        </div>
+      )}
     </div>
   );
 }
